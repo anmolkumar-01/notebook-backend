83449ad706bb730b71c00595c4db3ccfd506bcce
Anmol Kumar <anmolkumar.cod01@gmail.com>
2025-06-17 23:45:08 +0530

    added notesSchema

---
+++ src/models/notesSchema.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const notesSchema = new mongoose.Schema(
+    {
+
+        title: {
+            type: String,
+            required: [true, "Title is required"],
+        },
+        description: {
+            type: String,
+            default: "",
+        },
+        data: {
+            type: String,
+            default: "",
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: [true, "Owner is required"],
+        },
+        users:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+
+    } , 
+    {timestamps: true}
+);
+
+export const Notes = mongoose.model("Notes", notesSchema);
